43805dca01b3d192e82c9b025432d15bc68e0f6b
vincenttommi <vincenttommi@gmail.com>
2023-06-23 06:09:01 +0300

    lab completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,29 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
-
+    
     def get_response_body(self):
-        pass
-
+        response = requests.get(self.url)
+        return response.text
+    
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        parsed_data = None
+        
+        while not parsed_data:
+            try:
+                parsed_data = json.loads(response_body)
+                if not isinstance(parsed_data, (list, dict)):
+                    print("The response body is not a valid JSON.")
+            except json.JSONDecodeError:
+                pass
+        
+        return parsed_data
+
+url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+requester = GetRequester(url)
+data = requester.load_json()
+print(data)
+
